9db37ee280991851fbd20a1026cf7784c114cda1
PRAKASH-SK <prakashpv.cs24@bitsathy.ac.in>
2026-04-22 14:03:05 +0530

    center the board and increase the size of the board

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const scoreEl=document.querySelector('#scoreEl');
 const canvas=document.querySelector("canvas");
 const c=canvas.getContext("2d");
 
-canvas.width=1024;
-canvas.height=576;
+canvas.width=1250;
+canvas.height=700;
 
 let player=null;
 let particles=[];
